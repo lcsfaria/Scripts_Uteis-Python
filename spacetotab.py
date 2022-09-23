3375052028894a07723cc6b3f0057ed7706b9b63
lcsfaria <73356412+lcsfaria@users.noreply.github.com>
2022-09-22 23:53:58 -0300

    Update spacetotab.py

--- spacetotab.py
+++ spacetotab.py
@@ -3,5 +3,5 @@ arq = open('C:\\Users\\lucasf\\Desktop\\arquivoInicial.txt','r')
 vet = []
 for i in arq:
     vet.append(i.split())
-df = pd.DataFrame(vet2)
+df = pd.DataFrame(vet)
 df.to_csv('C:\\Users\\lucasf\\Desktop\\arquivoFinal.txt', sep='\t', index=False, header=0)

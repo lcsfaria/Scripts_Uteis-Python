9fdb09709e2559ea56aa18d3bdcaae0f27ae4797
lcsfaria <73356412+lcsfaria@users.noreply.github.com>
2022-10-03 13:33:33 -0300

    Create mean_columns.py

---
+++ mean_columns.py
@@ -0,0 +1,27 @@
+"""
+2022
+@author: lucasf
+"""
+
+import pandas as pd
+dados = pd.read_csv('C:\\Users\\lucasf\\home', sep='\t')
+pops = []
+
+for i in dados['Pop']:
+    pops.append(i)
+
+pops = set(pops)
+
+total =[]
+for i in pops:
+    filtro= dados.loc[dados['Pop']==i]
+    k1 = filtro['K1'].mean()
+    k2 = filtro['K2'].mean()
+    k3 = filtro['K3'].mean()
+    k4 = filtro['K4'].mean()
+
+    valores = [i,k1,k2,k3,k4]
+    total.append(valores)
+
+df = pd.DataFrame(total, columns=['Pop','k1 mean','k2 mean','k3 mean','k4 mean'])
+df.to_csv('C:\\Users\\lucasf\\Desktop\\Dadosluca.txt', sep='\t',index= False )
